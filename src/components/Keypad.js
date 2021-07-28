5023fdc660ea766b6636dc4d3acd7dab4a60ed7b
rregalado17 <rregalado17@gmail.com>
2021-07-27 23:39:33 -0400

    react event listeners review

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from 'react';
+
+
+function handleClick() {
+    console.log("Entering password...") 
+} 
+
+function Keypad () {
+    return (
+        <div>
+            <input type="password" onChange={handleClick}></input>
+        </div>
+    )
+}
+
+export default Keypad; 
